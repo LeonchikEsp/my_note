fd7ebdc103a7e6d64d31d9b1cd34a009a5e6de81
Aleksandr Leonchik <Aleksandr_Leonchik@epam.com>
2017-12-20 20:28:35 +0300

    NoteEndpoint begin

--- src/main/java/com/epam/mynote/service/impl/NoteServiceImpl.java
+++ src/main/java/com/epam/mynote/service/impl/NoteServiceImpl.java
@@ -1,8 +1,9 @@
 package com.epam.mynote.service.impl;
 
-import com.epam.mynote.domain.Label;
-import com.epam.mynote.domain.User;
+import com.epam.mynote.domain.Note;
 import com.epam.mynote.repository.NoteRepository;
+import com.epam.mynote.repository.NotebookRepository;
+import com.epam.mynote.repository.UserRepository;
 import com.epam.mynote.service.NoteService;
 import org.springframework.stereotype.Service;
 
@@ -11,25 +12,42 @@ import java.util.List;
 @Service
 public class NoteServiceImpl implements NoteService {
 
+    private final NotebookRepository notebookRepository;
+
     private final NoteRepository noteRepository;
 
-    public NoteServiceImpl(NoteRepository noteRepository) {
+    private final UserRepository userRepository;
+
+    public NoteServiceImpl(NotebookRepository notebookRepository, NoteRepository noteRepository, UserRepository userRepository) {
+        this.notebookRepository = notebookRepository;
         this.noteRepository = noteRepository;
+        this.userRepository = userRepository;
     }
 
-    public Label save(Label label) {
+    @Override
+    public Note getNoteByIdAndUserId(Long noteId, Long userId) {
+        return null;
+//        return noteRepository.findNoteByIdAndNotebook_UserId()
+    }
+
+    @Override
+    public List<Note> getAllNotesByUserId(Long userId) {
+        return null;
+//        noteRepository.findNoteByIdAndNotebook_UserId()
+    }
+
+    @Override
+    public List<Note> getAllNotesByUserIdByNotebookId(Long userId, Long notebookId) {
+        return noteRepository.findAllByNotebook_IdAndNotebook_User_Id(notebookId, userId);
+    }
+
+    @Override
+    public Integer deleteNoteByIdByUserId(Long id, Long userId) {
         return null;
     }
 
-    public Label save(Long userId, Label label) {
-        return null;
-    }
-
-    public Label update(Long labelId) {
-        return null;
-    }
-
-    public List<Label> getLabelsByUser(User user) {
+    @Override
+    public Note saveNoteByNotebookIdByUserId(Note note, Long notebookId, Long userId) {
         return null;
     }
 }
